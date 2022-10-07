49cadeee6c62e49541ff12820d4fef260efb698f
Andrew <anpilley@mycronite.net>
2022-10-07 15:47:28 -0700

    Removed some dead files.

--- PowerlessDeodorizers/copyBinary.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-copy bin\Release\PowerlessDeodorizers.dll D:\Private\Documents\Klei\OxygenNotIncluded\mods\Dev\PowerlessDeodorizers
-copy mod_info.yaml D:\Private\Documents\Klei\OxygenNotIncluded\mods\Dev\PowerlessDeodorizers
-
-echo copied!
-pause
