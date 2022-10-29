659bd8d00586f97a0b8a4e46f5a565b946cd009a
Andrew <anpilley@mycronite.net>
2022-10-29 13:16:00 -0700

    License headers

--- GroupResources/Patches.cs
+++ GroupResources/Patches.cs
@@ -1,4 +1,19 @@
-﻿using HarmonyLib;
+﻿// Copyright © 2022 Andrew Pilley
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+// files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+// Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using HarmonyLib;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -357,7 +372,8 @@ namespace GroupResources
                 if (!materialHeaders.ContainsKey(category))
                 {
                     // defer creation until later, since copying the header now causes issues.
-                    __instance.StartCoroutine(MakeHeaders(category));
+                    if(__instance.gameObject.activeInHierarchy)
+                        __instance.StartCoroutine(MakeHeaders(category));
                 }
 
                 // Tack on an extra BG to regular resource entries, so we can shade that.
